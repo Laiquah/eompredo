ceb7db22653ba9d1d88cf28c5eb2dbf87c4b94ff
laiquah <cotielaiquah@gmail.com>
2023-06-29 16:36:07 +0200

    added

--- src/components/AdminComp.vue
+++ src/components/AdminComp.vue
@@ -1,51 +1,33 @@
 <template>
     <div>
-        <table class="table">
-            <thead>
-              <tr class="col" v-for="item in product" :key="item.id">
-                <th scope="col">#</th>
-                <th scope="col">Title</th>
-                <th scope="col">Image</th>
-                <th scope="col">Description</th>
-                <th scope="col">Price</th>
-                <th scope="col">Edit</th>
-                <th scope="col">Delete</th>
-            </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">{{ item.id }}</th>
-                <td>{{ item.title }}</td>
-                <td>{{ item.image }}</td>
-                <td>{{ item.description }}</td>
-                <td>{{ item.price }}</td>
-                <td>{{ item.edit }}</td>
-                <td>{{ item.delete }}</td>
-              </tr>
-            </tbody>
-          </table>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Title</th>
+            <th scope="col">Image</th>
+            <th scope="col">Description</th>
+            <th scope="col">Price</th>
+            <th scope="col">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
 </template>
 
 <script>
     export default {
-        data() {
-    return {
-      product: [],
-    };
-  },
-  methods: {
-    async fetchData() {
-      const res = await fetch(
-        "https://laiquah.github.io/products/products.json"
-      );
-      let parsedProducts = await res.json();
-      this.product = parsedProducts.products;
-    },
-  },
-  mounted() {
-    this.fetchData();
-  },
+       
     }
 </script>
 
